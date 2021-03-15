683eb308ac211563105cfc23bfc18f3b422df276
Naveen Kumar <kumarn.na20@gmail.com>
2021-03-15 10:07:01 +0530

    Assignment complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,15 @@ export class AppComponent {
   title = 'first-app';
   elementVisibility = false;
   items = ['car', 'train', 'bus', 'bike', 'airplane'];
+  status: boolean = false;
+  name = "Default Value";
 
   toggleElementVisibility() {
     this.elementVisibility = !this.elementVisibility;
+  }
+
+  clickEvent(){
+    this.status = !this.status;       
   }
 
 }
