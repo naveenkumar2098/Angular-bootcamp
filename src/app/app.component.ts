8559832ab548e5c5a218823a537d47feb4e5a7bc
Naveen Kumar <kumarn.na20@gmail.com>
2021-03-26 11:38:15 +0530

    Added Assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient }  from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'first-app';
+  todaydate = new Date();
+  demoString = "This is demo of pipes, custom Pipe demo";
+  reverseString = "Demo of Reverse string custom pipe";
+  vowelString: string = `Let's checkout how much vowels this string contains`;
+  loadedPosts: any = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(){
+    this.fetchPosts();
+  }
+
+  onfetchPosts(){
+    this.fetchPosts();
+  }
+
+  private fetchPosts(){
+    this.http.get('https://jsonplaceholder.typicode.com/posts/1/comments').subscribe(posts => {
+      this.loadedPosts=posts;
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReversePipe } from './reverse.pipe';
+import { CountVowel } from './count.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReversePipe,
+    CountVowel
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
