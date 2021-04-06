a5cca22f9e3becf8d0cd5bb4b01d9fc64fcf8153
Naveen Kumar <kumarn.na20@gmail.com>
2021-04-07 00:19:10 +0530

    Added Assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first-app';
+  @ViewChild ('f') signup="NgForm";
+  
+  constructor(private router: Router) {}
+
+  onSubmit(){
+      this.router.navigate(['/dashboard']);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,55 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+import { AuthGuard } from './auth-guard.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { ResolveService } from './resolver';
+
+import { StoreModule } from '@ngrx/store';
+import { reducer } from './reducer/demo.reducer';
+import {ReadComponent } from './read/read.component';
+import {CreateComponent } from './create/create.component';
+
+
+const appRoutes : Routes = [
+  {
+    path: 'dashboard', component:DashboardComponent, canActivate:[AuthGuard],resolve:{news:ResolveService}
+  },
+  {
+    path: 'login',component:AppComponent, pathMatch:'full'
+  },
+  {
+    path: '404',component:NotfoundComponent
+  }
+  
+  
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    NotfoundComponent,
+    ReadComponent,
+    CreateComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    HttpClientModule,
+    StoreModule.forRoot({
+      tutorial: reducer
+    }),
+  
+    
   ],
-  providers: [],
+  providers: [ResolveService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
